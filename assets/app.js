210ee1c035826765026fa645ffc0509968bca3b2
michael mariea <mariea.michael@gmail.com>
2019-07-27 15:05:27 -0700

    started project

---
+++ assets/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    // const url = "https://cloud.iexapis.com/?&apikey=pk_b913cbce5cbb428eb5a5bbcba33eb6da"
+
+    // $.ajax({
+    //     url: url,
+    //           method: "GET"
+    //         }).then(function(response){
+    //             console.log(response);
+    //         });
+    $('#submit').on("click", function () {
+        let dval = $('#val').val();
+        console.log(dval);
+/// prices
+const ford = 10.23;
+const ge = 10.50;
+const apple = 197.92;
+const disney = 139.64;
+// months 
+
+
+
+    })
+});
