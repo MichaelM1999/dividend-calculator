39844fdb4f12605d08a12201d232606cc8359598
michael mariea <mariea.michael@gmail.com>
2019-08-03 10:05:16 -0700

    most recent stock day works

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
     todaysdate = [year, month, day].join('-');
     console.log(todaysdate);
     const dayofweek = moment().format("dddd");
+    console.log(dayofweek);
     $('#submit').on("click", function () {
         let dval = $('#val').val();
         console.log(dval);
@@ -18,21 +19,30 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
             console.log(response)
-            let closeC = response["Time Series (Daily)"][todaysdate]["4. close"];
-            let volume = response["Time Series (Daily)"][todaysdate]["6. volume"];
-
-            if (dayofweek === "sunday") {
-                recentday = (today.getDate() - 2);
-                mostrecentdate = [year, month, recentday].join('-');
-                closeC = response["Time Series (Daily)"][mostrecentdate]["4. close"];
-                let volume = response["Time Series (Daily)"][mostrecentdate]["6. volume"];
+            // console.log(moment().subtract(2, 'days').format("DD"));
+            if (dayofweek === "Monday" || dayofweek === "Tuesday" || dayofweek === "Wednesday" || dayofweek === "Thursday" || dayofweek === "Friday") {
+                let closeC = response["Time Series (Daily)"][todaysdate]["4. close"];
+                let volume = response["Time Series (Daily)"][todaysdate]["6. volume"];
+                console.log(closeC);
+                console.log(volume);
             }
-
-            if (dayofweek === "saturday") {
-                recentday = (today.getDate() - 1);
+            if (dayofweek === "Sunday") {
+                recentday = (moment().subtract(2, 'days').format("DD"));
                 mostrecentdate = [year, month, recentday].join('-');
-                closeC = response["Time Series (Daily)"][mostrecentdate]["4. close"];
+                console.log(mostrecentdate);
+                let closeC = response["Time Series (Daily)"][mostrecentdate]["4. close"];
                 let volume = response["Time Series (Daily)"][mostrecentdate]["6. volume"];
+                console.log(closeC);
+                console.log(volume);
+            }
+            if (dayofweek === "Saturday") {
+                recentday = (moment().subtract(1, 'days').format("DD"));
+                mostrecentdate = [year, month, recentday].join('-');
+                console.log(mostrecentdate);
+                let closeC = response["Time Series (Daily)"][mostrecentdate]["4. close"];
+                let volume = response["Time Series (Daily)"][mostrecentdate]["6. volume"];
+                console.log(closeC);
+                console.log(volume);
             }
 
             console.log(closeC);
